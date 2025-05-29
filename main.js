973b54c6b4c6e14944fd1d8a022fd55c72375f06
Monicamontes <montesmonica909@gmail.com>
2025-05-28 19:56:47 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+let menu_desplegable=document.getElementById('menu');
+let boton_menu=document.getElementById('x');
+function abrir_cerrar_menu(){
+    menu_desplegable.classList.toggle('abrir_menu');
+    boton_menu.classList.toggle('color_x');
+}
